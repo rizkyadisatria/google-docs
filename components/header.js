2ade14e83f8bf962286f3331c2ac870a6130ad27
rizkyadisatria <godfreyiibritannia@gmail.com>
2021-07-26 10:28:52 +0800

    fix header

--- components/header.js
+++ components/header.js
@@ -5,11 +5,11 @@ import { signOut, useSession } from "next-auth/client"
 function header() {
     const [session] = useSession()
     return (
-        <header className="sticky top-0 z-50 flex items-center px-4 py-2 shadow-md bg-white">
+        <header className="sticky top-0 z-50 flex items-center px-2 py-2 shadow-md bg-white">
             <Icon name='description' size='5xl' color='blue'/>
             <h1 className='hidden md:inline-flex ml-2 mr-10 text-gray-700 text-2xl'>Docs</h1>
 
-            <div className='flex flex-grow items-center mx-3 px-2 py-2 bg-gray-100 
+            <div className='flex flex-grow items-center mx-2 px-2 py-2 bg-gray-100 
             text-gray-600 rounded-lg focus-within:text-gray-600 focus-within:shadow-md'>
                 <Icon name='search' size='2xl' color='gray'/>
                 <input type="text" placeholder='Search' className="flex-grow text-base bg-transparent outline-none ml-1"/>
@@ -29,7 +29,7 @@ function header() {
             <img
             onClick={signOut} 
             loading='lazy'
-            className='cursor-pointer h-12 w-12 rounded-full ml-2'
+            className='cursor-pointer h-12 w-12 rounded-full ml-1'
             src={session?.user?.image} alt='' />
 
         </header>
